74622dee718c1d35fbc29e01de8c9cb2d474bd35
Maxime Bures <maxime.bures@markentive.com>
2023-12-26 16:16:58 +0100

    Add modification on recap

--- src/components/cover_text/cover_text.js
+++ src/components/cover_text/cover_text.js
@@ -32,7 +32,7 @@ function Intro(props) {
 
 
 function Question1(props) {
-  const { onLeave, className, data } = props
+  const { onLeave, className, data, backToRecap } = props
   const [leave, setLeave] = useState(false)
   const [answer, setAnswer] = useState(data)
   const [show, setShow] = useState(false)
@@ -58,7 +58,7 @@ function Question1(props) {
               <Input value={answer} onChange={setAnswer} onKeyPress={handleKeypress} placeholder="Prénom Nom"/>
             <br/>
             {answer && (
-              <Button onClick={done} active={leave} key="done_q1">{"Suivant >"}</Button>
+              <Button onClick={done} active={leave} key="done_q1">{backToRecap ? "Retour au recapitulatif" : "Suivant"}</Button>
             )}
           </div>
         )
@@ -68,7 +68,7 @@ function Question1(props) {
 }
 
 function Question2(props) {
-  const { onLeave, className, data } = props
+  const { onLeave, className, data, backToRecap } = props
   const [leave, setLeave] = useState(false)
   const [answer, setAnswer] = useState(data)
   const [show, setShow] = useState(false)
@@ -96,7 +96,7 @@ function Question2(props) {
             <br/>
             {answer && (
             <Fade duration={1000}>
-              <Button onClick={done} active={leave} key="done_q2">Suivant</Button>
+              <Button onClick={done} active={leave} key="done_q2">{backToRecap ? "Retour au recapitulatif" : "Suivant"}</Button>
             </Fade>
             )}
           </div>
@@ -107,7 +107,7 @@ function Question2(props) {
 }
 
 function Question3(props) {
-  const { onLeave, className, data } = props
+  const { onLeave, className, data, backToRecap } = props
   const [leave, setLeave] = useState(false)
   const [answer, setAnswer] = useState(data)
   const [show, setShow] = useState(false)
@@ -136,7 +136,7 @@ function Question3(props) {
             <br/>
             {answer !== undefined && (
             <Fade duration={1000}>
-              <Button onClick={done} active={leave} key="done_q3">Suivant</Button>
+              <Button onClick={done} active={leave} key="done_q3">{backToRecap ? "Retour au recapitulatif" : "Suivant"}</Button>
             </Fade>
             )}
           </div>
@@ -147,7 +147,7 @@ function Question3(props) {
 }
 
 function Question4(props) {
-  const { onLeave, className, data } = props
+  const { onLeave, className, data, backToRecap } = props
   const [leave, setLeave] = useState(false)
   const [answer, setAnswer] = useState(data["answer"])
   const [instrument, setInstrument] = useState(data["instrument"])
@@ -193,7 +193,7 @@ function Question4(props) {
               <>
                 <br/>
                 <div style={{width: "100%", display: "flex", flexDirection: "column", alignItems: "center"}} className="quick-fade">
-                  <Button onClick={done} active={leave} key="done_q4">Suivant</Button>
+                  <Button onClick={done} active={leave} key="done_q4">{backToRecap ? "Retour au recapitulatif" : "Suivant"}</Button>
                 </div>
               </>
             )
@@ -215,11 +215,11 @@ function Recap(props) {
   const [show5, setShow5] = useState(false)
 
   useEffect(() => {
-    setTimeout(() => setShow1(true), 1500)
-    setTimeout(() => setShow2(true), 2500)
-    setTimeout(() => setShow3(true), 3500)
-    setTimeout(() => setShow4(true), 4500)
-    setTimeout(() => setShow5(true), 5500)
+    setTimeout(() => setShow1(true), 2500)
+    setTimeout(() => setShow2(true), 3500)
+    setTimeout(() => setShow3(true), 4500)
+    setTimeout(() => setShow4(true), 5500)
+    setTimeout(() => setShow5(true), 6500)
   }, [])
 
   const done = () => {
@@ -232,20 +232,25 @@ function Recap(props) {
       <div className="title" key="recap_title">
         <Fade key="000" className="fade-item" cascade duration={100} delay={300}>Recapitulons :</Fade>
       </div>
-      <br/>
-      { show1 && <div className="quick-fade" onClick={() => undefined}>Tu es <b>{data[1]}</b>.</div> }
-      { show2 && <div className="quick-fade" onClick={() => undefined}>Ton adresse pour le faire-part est <b>{data[2]}</b>.</div> }
-      { show3 && <div className="quick-fade" onClick={() => undefined}>Tu <b>{data[3] ? "souhaites" : "ne souhaites pas"}</b> chanter dans la chorale.</div> }
-      { show4 && (data[4]["answer"]
-        ? <div className="quick-fade" onClick={() => undefined}>Tu <b>souhaites</b> jouer d'un instrument à la messe (<b>{data[4]["instrument"]}</b>).</div>
-        : <div className="quick-fade" onClick={() => undefined}>Tu <b>ne souhaites pas</b> jouer d'un instrument à la messe.</div>
-      )}
-      <br/>
-      { show5 && (
-      <div style={{width: "100%", display: "flex", flexDirection: "column", alignItems: "center"}} className="quick-fade">
-        <Button onClick={done} active={leave} key="done_recap">Terminer</Button>
+      <div className="recap-instructions" key="recap_instructions">
+        <Fade key="002" className="fade-item" cascade duration={100} delay={800}>Clique sur une ligne pour modifier</Fade>
       </div>
-      ) }
+      <br/>
+      <div style={{display: "flex", flexDirection: "column", rowGap: "1vh"}}>
+        { show1 && <div className="quick-fade" onClick={() => onLeave(1)}>Tu es <b>{data[1]}</b>.</div> }
+        { show2 && <div className="quick-fade" onClick={() => onLeave(2)}>Ton adresse pour le faire-part est <b>{data[2]}</b>.</div> }
+        { show3 && <div className="quick-fade" onClick={() => onLeave(3)}>Tu <b>{data[3] ? "souhaites" : "ne souhaites pas"}</b> chanter dans la chorale.</div> }
+        { show4 && (data[4]["answer"]
+          ? <div className="quick-fade" onClick={() => onLeave(4)}>Tu <b>souhaites</b> jouer d'un instrument à la messe (<b>{data[4]["instrument"]}</b>).</div>
+          : <div className="quick-fade" onClick={() => onLeave(4)}>Tu <b>ne souhaites pas</b> jouer d'un instrument à la messe.</div>
+        )}
+        <br/>
+        { show5 && (
+        <div style={{width: "100%", display: "flex", flexDirection: "column", alignItems: "center"}} className="quick-fade">
+          <Button onClick={done} active={leave} key="done_recap">Terminer</Button>
+        </div>
+        ) }
+      </div>
     </div>
   )
 }
@@ -268,6 +273,7 @@ function CoverText(props) {
   const [back, setBack] = useState(false)
   const [next, setNext] = useState(false)
   const [previous, setPrevious] = useState(false)
+  const [backToRecap, setBackToRecap] = useState(false)
   const [data, setData] = useState({
     1: "",
     2: "",
@@ -279,6 +285,15 @@ function CoverText(props) {
   })
 
   const changeStage = (value = undefined, previous = false) => {
+    if (value !== undefined && stage === 5) {
+      setPrevious(true)
+      setTimeout(() => {
+        setBackToRecap(true)
+        setStage(value)
+        setPrevious(false)
+      }, 1500)
+      return
+    }
     if (value !== undefined) {
       const new_data = data
       new_data[stage] = value
@@ -295,7 +310,12 @@ function CoverText(props) {
       setNext(true)
       setTimeout(() => {
         setBack(false)
-        setStage(stage + 1)
+        if (backToRecap) {
+          setStage(5)
+          setBackToRecap(false)
+        }
+        else
+          setStage(stage + 1)
         setNext(false)
       }, 1500)
     }
@@ -304,10 +324,10 @@ function CoverText(props) {
   
   const stages = [
     <Intro className={`${next ? "next" : ""}${previous ? "previous" : ""}`} key="00" onLeave={changeStage} />,
-    <Question1 data={data[1]} className={`${next ? "next" : ""}${previous ? "previous" : ""}`} key="01" onLeave={changeStage} />,
-    <Question2 data={data[2]} className={`${next ? "next" : ""}${previous ? "previous" : ""}`} key="02" onLeave={changeStage} />,
-    <Question3 data={data[3]} className={`${next ? "next" : ""}${previous ? "previous" : ""}`} key="03" onLeave={changeStage} />,
-    <Question4 data={data[4]} className={`${next ? "next" : ""}${previous ? "previous" : ""}`} key="04" onLeave={changeStage} />,
+    <Question1 backToRecap={backToRecap} data={data[1]} className={`${next ? "next" : ""}${previous ? "previous" : ""}`} key="01" onLeave={changeStage} />,
+    <Question2 backToRecap={backToRecap} data={data[2]} className={`${next ? "next" : ""}${previous ? "previous" : ""}`} key="02" onLeave={changeStage} />,
+    <Question3 backToRecap={backToRecap} data={data[3]} className={`${next ? "next" : ""}${previous ? "previous" : ""}`} key="03" onLeave={changeStage} />,
+    <Question4 backToRecap={backToRecap} data={data[4]} className={`${next ? "next" : ""}${previous ? "previous" : ""}`} key="04" onLeave={changeStage} />,
     <Recap className={`${next ? "next" : ""}${previous ? "previous" : ""}`} data={data} key="05" onLeave={changeStage} />,
     <Thank className={`${next ? "next" : ""}${previous ? "previous" : ""}`} key="06" />,
   ]
@@ -315,7 +335,7 @@ function CoverText(props) {
   return (
       <div className={`cover-text`}>
         {
-          back && stage > 1 && stage <= 5 && (
+          back && !backToRecap && stage > 1 && stage < 5 && (
             <div id="back-button" className={(next || previous) ? "button-out" : "button-in"}>
               <Button onClick={() => changeStage(undefined, true)} active={false} key="previous">{"<"}</Button>
             </div>
